c4bcea2f9f24f41f9b37c122788596e41865b82e
Patrice GADEGBE <gadegbea@gmail.com>
2020-06-18 19:15:51 +0200

    set server to localhost if blank input text

--- mobile_app/LO53Lab/app/src/main/java/master/iot/lo53lab/MainActivity.java
+++ mobile_app/LO53Lab/app/src/main/java/master/iot/lo53lab/MainActivity.java
@@ -2,6 +2,9 @@ package master.iot.lo53lab;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -18,6 +21,7 @@ import okhttp3.Response;
 public class MainActivity extends AppCompatActivity {
     private TextView mTextViewResult;
     private EditText serverIPWidget;
+    private String macAddress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,11 +30,21 @@ public class MainActivity extends AppCompatActivity {
 
         mTextViewResult = findViewById(R.id.requestResult);
         serverIPWidget = findViewById(R.id.serverIP);
-            }
+
+        WifiManager manager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+        WifiInfo info = manager.getConnectionInfo();
+        macAddress = info.getMacAddress();
+
+        mTextViewResult.setText(macAddress);
+
+
+    }
 
     public void startCalibrationRequest(View view){
         String serverIp = String.valueOf(serverIPWidget.getText());
-        String macAddress = "";
+        if(serverIp.equals("")){
+            serverIp = "http://localhost:5000";
+        }
         double x=0;
         double y=0;
         double z=0;
@@ -43,14 +57,18 @@ public class MainActivity extends AppCompatActivity {
 
     public void stopCalibrationRequest(View view){
         String serverIp = String.valueOf(serverIPWidget.getText());
-        String macAddress = "";
+        if(serverIp.equals("")){
+            serverIp = "http://localhost:5000";
+        }
         String url = serverIp + "/stop_calibration?mac_addr="+ macAddress ;
         sendRequest(url);
     }
 
     public void locateRequest(View view){
         String serverIp = String.valueOf(serverIPWidget.getText());
-        String macAddress = "";
+        if(serverIp.equals("")){
+            serverIp = "http://localhost:5000";
+        }
         String url = serverIp + "/locate?mac_addr="+ macAddress ;
         sendRequest(url);
     }
